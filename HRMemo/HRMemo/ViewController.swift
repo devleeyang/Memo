c222be1eb6bb76bbd986751585d57f0ccfd18632
양혜리 <yangyang6424@gmail.com>
2018-11-19 19:20:31 +0900

    creat xcode project

---
+++ HRMemo/HRMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HRMemo
+//
+//  Created by 양혜리 on 19/11/2018.
+//  Copyright © 2018 양혜리. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
