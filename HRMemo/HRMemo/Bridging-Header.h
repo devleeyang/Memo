bbf0628321a4d63ad7b2fbe03533185c1026017d
양혜리 <yangyang6424@gmail.com>
2018-11-19 21:17:35 +0900

    add fmdb in bridging-header because 3rd party language is Objc

---
+++ HRMemo/HRMemo/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  HRMemo
+//
+//  Created by 양혜리 on 19/11/2018.
+//  Copyright © 2018 양혜리. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "FMDB.h"
+
+#endif /* Bridging_Header_h */
