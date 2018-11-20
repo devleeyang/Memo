20287b07eb843372373ebc1ce35d23cc21d42e8b
양혜리 <yangyang6424@gmail.com>
2018-11-20 19:11:17 +0900

    init write view

---
+++ HRMemo/HRMemo/HRWriteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HRWriteViewController.swift
+//  HRMemo
+//
+//  Created by 양혜리 on 20/11/2018.
+//  Copyright © 2018 양혜리. All rights reserved.
+//
+
+import UIKit
+
+class HRWriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
